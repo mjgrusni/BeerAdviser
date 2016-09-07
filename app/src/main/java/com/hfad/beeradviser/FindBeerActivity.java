6129263c57f6af1304ddd2c69fb39654f9ca0b7f
mjgrusni <mjgrusni@svsu.edu>
2016-09-07 15:59:29 -0400

    End of class, BeerExpert file added.

--- app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
+++ app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
@@ -2,6 +2,13 @@ package com.hfad.beeradviser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FindBeerActivity extends AppCompatActivity {
 
@@ -10,4 +17,34 @@ public class FindBeerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
     }
+
+    // Call when button is clicked
+    public void onClickFindBeer(View view) {
+
+        // Get reference to the TextView
+        TextView brands = (TextView) findViewById(R.id.brands);
+
+        // Get reference to Spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+
+        // Get selected line item in Spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+
+        BeerExpert expert = new BeerExpert();
+
+        // Get recommendations from BeerExpert class
+        List<String> brandsList = expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for (String brand: brandsList){
+            brandsFormatted.append(brand).append('\n');
+        }
+
+       // String zippydee = new BigInteger(130, random).toString();
+
+        // Display beers
+        //  brands.setText(beerType);
+        brands.setText(brandsFormatted);
+
+    }
 }
+
